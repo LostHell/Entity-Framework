03c502a3345f6dfb5dd0cd3b0ad956d788a6b06b
E.Karadzhov <losthell92@gmail.com>
2019-11-28 14:58:34 +0100

    Upload Design Patterns

---
+++ Homeworks/10. EXERCISE DESIGN PATTERNS/Template Pattern/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Template_Pattern
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Sourdough sourdough = new Sourdough();
+            sourdough.Make();
+
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+        }
+    }
+}
